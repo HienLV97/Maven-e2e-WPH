8b28d63ddb7cc695864275be9b42de383ad03c0f
HienLV97 <levinhhien2603@gmail.com>
2024-01-16 22:12:30 +0700

    a

--- src/main/java/Support/Constants.java
+++ src/main/java/Support/Constants.java
@@ -1,32 +1,27 @@
 package Support;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import static Support.Initialization.Init.driver;
-public class Constants {
-//     WebDriver driver = null;
 
-//    public Constants(WebDriver driver) {
-//        this.driver = driver;
-//    }
+public class Constants {
 
 	public static String emailAccount = "kamoratest1@g.c";
 	public static String passAccount = "iamafriend";
 	public String YetiCMS = "https://yeti-cms.dev/api";
 	public String YetiCMSToken = "2b0c615afb1b72cf093a5fa6d48c7ef1";
+
 	public static WebElement getPlaceholder(String value) {
 		return driver.findElement(By.xpath("//input[contains(@placeholder, '" + value + "')]"));
 	}
 
 	public static WebElement getBtn(String value) {
 		return driver.findElement(By.xpath("//*/button[contains(text(), '" + value + "')]"));
-//                                                          button[contains(text(),'Login')]
 	}
+
 	public WebElement getText(String value) {
-//        return driver.findElement(By.xpath("//*[contains(text(), '" + value + "')]"));
-		return driver.findElement(By.xpath("//*[contains(text(), '"+value+"')]"));
+		return driver.findElement(By.xpath("//*[contains(text(), '" + value + "')]"));
 	}
 
 }
